b1ac87e704a4c22280744b88600a9a0893425e24
grasword <grasword6@gmail.com>
2022-11-07 23:43:33 +0000

    added gitignore
    project setup
    
    
    installed `Tailwind`
    
    
    commented out `React.StrictMode`
    
    
    created the `Product` component
    
    
    imported `Product` component to the `App`

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Product } from './components/Product'
+import './App.css'
+
+function App() {
+  return (
+    <div className="container mx-auto max-w-2xl pt-5">
+      <Product />
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/Product.tsx
@@ -0,0 +1,3 @@
+export function Product() {
+  return <div className="border py-2 px-4 rounded flex flex-col items-center mb-2">Product!</div>
+}
