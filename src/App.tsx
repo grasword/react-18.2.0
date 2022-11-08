b5b8eb6c32a1f699657455e79329a373b44013e4
grasword <grasword6@gmail.com>
2022-11-08 23:06:39 +0000

    moved all the logic from `App` to the `product` hooks

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,8 @@
-import React, { useEffect, useState } from 'react'
 import { Product } from './components/Product'
-import './App.css'
-import axios, { Axios, AxiosError } from 'axios'
-import { IProduct } from './models'
+import { useProducts } from './hooks/products'
 
 function App() {
-  const [products, setProducts] = useState<IProduct[]>([])
-  const [loading, setLoading] = useState(false)
-  const [error, setError] = useState('')
-
-  async function fetchProducts() {
-    try {
-      setError('')
-      setLoading(true)
-      const response = await axios.get<IProduct[]>('https://fakestoreapi.com/products?limit=10')
-      setProducts(response.data)
-      setLoading(false)
-    } catch (e: unknown) {
-      const error = e as AxiosError
-      setLoading(false)
-      setError(error.message)
-    }
-  }
-
-  useEffect(() => {
-    fetchProducts()
-  }, [])
+  const { products, loading, error } = useProducts()
 
   return (
     <div className="container mx-auto max-w-2xl pt-5">
