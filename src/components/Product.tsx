44600a379a0cc60cac9947afafa23a981bd61468
grasword <grasword6@gmail.com>
2022-11-08 22:16:56 +0000

    implemented dynamic data injection into the`Product` component

--- src/components/Product.tsx
+++ src/components/Product.tsx
@@ -1,9 +1,29 @@
+import { useState } from 'react'
 import { IProduct } from '../models'
 
 interface ProductProps {
   product: IProduct
 }
 
-export function Product(props: ProductProps) {
-  return <div className="border py-2 px-4 rounded flex flex-col items-center mb-2">{props.product.title}</div>
+export function Product({ product }: ProductProps) {
+  const [details, setDetails] = useState(false)
+
+  const btnClassName = details ? 'bg-blue-400' : 'bg-yellow-400'
+
+  const btnClasses = ['py-2 px-4 border', btnClassName]
+  return (
+    <div className="border py-2 px-4 rounded flex flex-col items-center mb-2">
+      <img src={product.image} className="w-1/6" alt={product.title}></img>
+      <p>{product.title}</p>
+      <span className="font-bold">{product.price}</span>
+      <button className={btnClasses.join(' ')} onClick={() => setDetails(!details)}>
+        {details ? 'Hide Details' : 'Show Details'}
+      </button>
+      {details && (
+        <div>
+          <p>{product.description}</p>
+        </div>
+      )}
+    </div>
+  )
 }
