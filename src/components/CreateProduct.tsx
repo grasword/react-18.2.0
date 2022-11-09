e6a86e3c2affced0a8cd268b90c4a9f0618b7b42
grasword <grasword6@gmail.com>
2022-11-09 10:51:51 +0000

    implemented functionality of creating the new product

--- src/components/CreateProduct.tsx
+++ src/components/CreateProduct.tsx
@@ -1,7 +1,54 @@
+import axios from 'axios'
+import { useState } from 'react'
+import { IProduct } from '../models'
+import { ErrorMessage } from './ErrorMessage'
+
+const productData: IProduct = {
+  title: 'test product',
+  price: 13.5,
+  description: 'lorem ipsum set',
+  image: 'https://i.pravatar.cc',
+  category: 'electronic',
+  rating: {
+    count: 20,
+    rate: 5
+  }
+}
+
 export function CreateProduct() {
+  const [value, setValue] = useState('')
+  const [error, setError] = useState('')
+
+  const submitHandler = async (event: React.FormEvent) => {
+    event.preventDefault()
+    setError('')
+
+    if (value.trim().length === 0) {
+      setError('Please enter valid title')
+      return
+    }
+    productData.title = value
+
+    const responce = await axios.post<IProduct>('https://fakestoreapi.com/products', productData)
+  }
+
+  const changeHandler = (event: any) => {
+    // event: React.KeyboardEvent < HTMLInputElement >
+    setValue(event.target.value)
+  }
+
   return (
-    <form>
-      <input type="text" className="border py-2 px-4 mb-2 w-full outline-0" placeholder="Enter product title..." />
+    <form onSubmit={submitHandler}>
+      <input
+        type="text"
+        className="border py-2 px-4 mb-2 w-full outline-0"
+        placeholder="Enter product title..."
+        value={value}
+        onChange={changeHandler}
+      />
+
+      {error && <ErrorMessage error={error} />}
+
       <button type="submit" className="py-2 px-4 border bg-yellow-400 hover:text-white">
         Create
       </button>
