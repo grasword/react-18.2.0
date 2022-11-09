81d5ca7505a6d44e39a910373a885b356a5d4c5d
grasword <grasword6@gmail.com>
2022-11-09 11:47:15 +0000

    added usage of `ModalContext` in the `App`

--- src/App.tsx
+++ src/App.tsx
@@ -4,15 +4,16 @@ import { Product } from './components/Product'
 import { useProducts } from './hooks/products'
 import { Modal } from './components/Modal'
 import { CreateProduct } from './components/CreateProduct'
-import { useState } from 'react'
+import { useContext } from 'react'
 import { IProduct } from './models'
+import { ModalContext } from './context/ModalContext'
 
 function App() {
   const { products, loading, error, addProduct } = useProducts()
-  const [modal, setModal] = useState(false)
+  const { modal, open, close } = useContext(ModalContext)
 
   const createHandler = (product: IProduct) => {
-    setModal(false)
+    close()
     addProduct(product)
   }
 
@@ -25,13 +26,13 @@ function App() {
       ))}
 
       {modal && (
-        <Modal title="Create New Product" onClose={() => setModal(false)}>
+        <Modal title="Create New Product" onClose={close}>
           <CreateProduct onCreate={createHandler} />
         </Modal>
       )}
       <button
         className="fixed bottom-5 right-5 rounded-full bg-yellow-500 text-white text-2xl px-4 py-1.5"
-        onClick={() => setModal(true)}
+        onClick={open}
       >
         +
       </button>
