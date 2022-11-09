7ed54d736f3c9a877fa055334f8a033d1d845afb
grasword <grasword6@gmail.com>
2022-11-09 14:21:07 +0000

    added `About` page

---
+++ src/pages/AboutPage.tsx
@@ -0,0 +1,10 @@
+export function AboutPage() {
+  return (
+    <div>
+      Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptas porro consequatur, eligendi vitae quo
+      aspernatur ad incidunt perspiciatis aliquam, at nostrum quas voluptates eveniet illo quos, corrupti mollitia
+      consectetur? Corrupti impedit quia vel amet atque aliquam alias, iste consectetur molestiae vero voluptatem
+      recusandae dolores illum earum fugiat inventore. Eligendi, rem!
+    </div>
+  )
+}
