f14755bc0d22c1f78931f14e1e7866d9bd0d017e
grasword <grasword6@gmail.com>
2022-11-09 14:20:55 +0000

    implemented routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,42 +1,13 @@
-import { Loader } from './components/Loader'
-import { ErrorMessage } from './components/ErrorMessage'
-import { Product } from './components/Product'
-import { useProducts } from './hooks/products'
-import { Modal } from './components/Modal'
-import { CreateProduct } from './components/CreateProduct'
-import { useContext } from 'react'
-import { IProduct } from './models'
-import { ModalContext } from './context/ModalContext'
+import { Route, Routes } from 'react-router-dom'
+import { AboutPage } from './pages/AboutPage'
+import { ProductPage } from './pages/ProductsPage'
 
 function App() {
-  const { products, loading, error, addProduct } = useProducts()
-  const { modal, open, close } = useContext(ModalContext)
-
-  const createHandler = (product: IProduct) => {
-    close()
-    addProduct(product)
-  }
-
   return (
-    <div className="container mx-auto max-w-2xl pt-5">
-      {loading && <Loader />}
-      {error && <ErrorMessage error={error} />}
-      {products.map((product) => (
-        <Product product={product} key={product.id} />
-      ))}
-
-      {modal && (
-        <Modal title="Create New Product" onClose={close}>
-          <CreateProduct onCreate={createHandler} />
-        </Modal>
-      )}
-      <button
-        className="fixed bottom-5 right-5 rounded-full bg-yellow-500 text-white text-2xl px-4 py-1.5"
-        onClick={open}
-      >
-        +
-      </button>
-    </div>
+    <Routes>
+      <Route path="/" element={<ProductPage />} />
+      <Route path="/about" element={<AboutPage />} />
+    </Routes>
   )
 }
 
--- src/index.tsx
+++ src/index.tsx
@@ -4,12 +4,15 @@ import './index.css'
 import App from './App'
 import reportWebVitals from './reportWebVitals'
 import { ModalState } from './context/ModalContext'
+import { BrowserRouter } from 'react-router-dom'
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 root.render(
-  <ModalState>
-    <App />
-  </ModalState>
+  <BrowserRouter>
+    <ModalState>
+      <App />
+    </ModalState>
+  </BrowserRouter>
 )
 
 // If you want to start measuring performance in your app, pass a function
