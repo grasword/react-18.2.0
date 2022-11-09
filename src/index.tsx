7cae847664957f6f0ee0ddd5f4e27fc930517bfc
grasword <grasword6@gmail.com>
2022-11-09 11:46:20 +0000

    put `App` component into the `ModalState`

--- src/index.tsx
+++ src/index.tsx
@@ -3,12 +3,13 @@ import ReactDOM from 'react-dom/client'
 import './index.css'
 import App from './App'
 import reportWebVitals from './reportWebVitals'
+import { ModalState } from './context/ModalContext'
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 root.render(
-  // <React.StrictMode>
-  <App />
-  // </React.StrictMode>
+  <ModalState>
+    <App />
+  </ModalState>
 )
 
 // If you want to start measuring performance in your app, pass a function
