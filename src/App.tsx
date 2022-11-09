c58775c84803e361fcb9c88d24868770045864fa
grasword <grasword6@gmail.com>
2022-11-09 11:31:29 +0000

    added the button to open modal window on click

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,7 @@ import { IProduct } from './models'
 
 function App() {
   const { products, loading, error, addProduct } = useProducts()
-  const [modal, setModal] = useState(true)
+  const [modal, setModal] = useState(false)
 
   const createHandler = (product: IProduct) => {
     setModal(false)
@@ -25,10 +25,16 @@ function App() {
       ))}
 
       {modal && (
-        <Modal title="Create New Product">
+        <Modal title="Create New Product" onClose={() => setModal(false)}>
           <CreateProduct onCreate={createHandler} />
         </Modal>
       )}
+      <button
+        className="fixed bottom-5 right-5 rounded-full bg-yellow-500 text-white text-2xl px-4 py-1.5"
+        onClick={() => setModal(true)}
+      >
+        +
+      </button>
     </div>
   )
 }
